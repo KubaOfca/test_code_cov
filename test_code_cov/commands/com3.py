7bfd77ca505d23ad6447ddc4bbaa29cbdc6e42b5
Jakub Lechowski <b.kuba.13@gmail.com>
2022-11-30 13:26:29 +0100

    remove utils from sub function

--- test_code_cov/commands/com3.py
+++
@@ -1,6 +0,0 @@
-def cube(x):
-    return x ** 3
-
-
-def square(x):
-    return x ** 2
--- test_code_cov/commands/comand1.py
+++ test_code_cov/commands/comand1.py
@@ -1,8 +1,7 @@
 from test_code_cov import utils
-from test_code_cov.commands import com3
+
 def add(x,y):
     return x+y
 
 def sub_div(x,y):
-    print(utils.div(1, 2))
-    return x-y, com3.cube(2)
+    return x-y
