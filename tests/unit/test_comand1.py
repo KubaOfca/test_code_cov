d5b3f9300033b50c1a01c5e5c65bf67f30ab4386
Jakub Lechowski <b.kuba.13@gmail.com>
2022-11-30 11:58:43 +0100

    add test

--- tests/unit/test_comand1.py
+++ tests/unit/test_comand1.py
@@ -3,5 +3,5 @@ from test_code_cov.commands import comand1
 def test_add():
     assert 3 == comand1.add(1, 2)
 
-# def test_sub_div():
-#     assert 2 == comand1.sub_div(3, 1)
+def test_sub_div():
+    assert 2 == comand1.sub_div(3, 1)
