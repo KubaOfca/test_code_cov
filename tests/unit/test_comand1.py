f33083f16bf31333a803cd65205af33ca1ea9ae9
Jakub Lechowski <b.kuba.13@gmail.com>
2022-11-30 12:19:29 +0100

    add com2 test

--- tests/unit/test_comand1.py
+++ tests/unit/test_comand1.py
@@ -1,7 +1,10 @@
 from test_code_cov.commands import comand1
-
+from test_code_cov.commands import comand2
 def test_add():
     assert 3 == comand1.add(1, 2)
 
 def test_sub_div():
     assert 2 == comand1.sub_div(3, 1)
+
+def test_com2():
+    assert 2 == comand2.power(2, 1)
